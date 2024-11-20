1153debf254579e3c7340ad1f9fb20505865eeb8
NσσႦ Sƈɾιρƚҽɾ <58369054+ScripterSaurav@users.noreply.github.com>
2024-11-20 14:56:56 +0530

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,14 @@
 
-// Toggle Dark/Light Mode
-function toggleMode() {
-  const body = document.body;
-  const modeButton = document.querySelector('.mode-toggle');
+    // Toggle Dark/Light Mode
+    function toggleMode() {
+      const body = document.body;
+      const modeButton = document.querySelector('.mode-toggle i');
 
-  if (body.classList.contains('dark-mode')) {
-    body.classList.replace('dark-mode', 'light-mode');
-    modeButton.textContent = '🌞'; // Sun icon for light mode
-  } else {
-    body.classList.replace('light-mode', 'dark-mode');
-    modeButton.textContent = '🌙'; // Moon icon for dark mode
-  }
-}
+      if (body.classList.contains('dark-mode')) {
+        body.classList.replace('dark-mode', 'light-mode');
+        modeButton.classList.replace('fa-moon', 'fa-sun'); // Sun icon for light mode
+      } else {
+        body.classList.replace('light-mode', 'dark-mode');
+        modeButton.classList.replace('fa-sun', 'fa-moon'); // Moon icon for dark mode
+      }
+    }
